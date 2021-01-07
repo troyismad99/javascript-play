43daf7a5af902a1bf836eb1c99c37abd2ecf8077
troyismad99 <troyhamilton@gmail.com>
2021-01-06 17:40:20 -0800

    January Coding challenge. 2021.01.06 -> Kth Missing Positive Number

---
+++ leetcode/Challenges/2021-01-07-kthMissingInteger.js
@@ -0,0 +1,48 @@
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Find the kth positive integer that is missing from this array.
+
+Example 1:
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+Example 2:
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+ 
+Constraints:
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for 1 <= i < j <= arr.length
+*/
+
+function findKthPositive (arr, k) {
+
+    var missingCount = 0;
+
+    // instead of iterating through arr...
+    // ... start at 1 and count up, tracking what is missing
+    for (i=1; missingCount !=k; i++) {
+
+        if (!arr.includes(i)) {
+             missingCount++;
+        }
+
+        // done when we reach the Kth missing i
+        if (missingCount === k) return i;
+    }
+}
+
+var set1 = [2,3,4,7,11];
+var k = 5;
+console.log(findKthPositive(set1,k));
+
+
+var set2 = [1,2,3,4];
+var k = 2;
+console.log(findKthPositive(set2,k));
+
+console.log(findKthPositive([1,2,4,5,7,8],2));
